3fcecf6d6c6038bc9825ab06686ecce2617af173
Caridad Rivera <ccrive10@gmail.com>
2019-05-06 14:32:56 -0400

    WorkingBackend

---
+++ db/migrate/20190506141911_create_games.rb
@@ -0,0 +1,11 @@
+class CreateGames < ActiveRecord::Migration[5.2]
+  def change
+    create_table :games do |t|
+      t.integer :user_id
+      t.integer :score
+      t.string :difficulty
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Image.create(name: "Red Diamond", img_url: "https://banner2.kisspng.com/20180418/cdq/kisspng-playing-card-game-suit-clip-art-diamonds-vector-5ad7ef53a0bfd4.8688200515241009476584.jpg")
+Image.create(name: "Green Clover", img_url: "https://www.pinclipart.com/picdir/middle/15-152865_four-leaf-clover-4-leaf-clover-four-pictures.png")
