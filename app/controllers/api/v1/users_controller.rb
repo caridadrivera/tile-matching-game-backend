59aa41654288d43639d0535a316222d8e177f018
Caridad Rivera <ccrive10@gmail.com>
2019-05-08 18:34:57 -0400

    corrected backend

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,17 +1,18 @@
 class Api::V1::UsersController < ApplicationController
 
   def index
-    @users = User.all
-    render json: @users
+    users = User.all
+    render json: users
   end
 
-  def new
-  end
+  # def new
+  #   user = User.new
+  # end
 
   def create
     # byebug
-    @user = User.create(user_params)
-    render json: @user.name
+    user = User.create(user_params)
+    render json: user
   end
 
   private
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,17 @@ Image.create(name: "Cereal", img_url: "https://www.kiss917.com/wp-content/upload
 Image.create(name: "Razr", img_url: "https://ksassets.timeincuk.net/wp/uploads/sites/54/2019/01/motorola-razr-920x734.jpg")
 Image.create(name: "Dicey", img_url: "https://images-na.ssl-images-amazon.com/images/I/81xGyIWqgCL._SX425_.jpg")
 Image.create(name: "Mr.Krabbs", img_url: "https://ih0.redbubble.net/image.667799040.5161/flat,1000x1000,075,f.u1.jpg")
-# Image.create(name: "Cereal", img_url: "https://www.kiss917.com/wp-content/uploads/sites/4/2017/05/cereal-952x576.jpg")
+User.create(name: "Herman")
+User.create(name: "Tito")
+User.create(name: "Luis")
+User.create(name: "Jack")
+
+Game.create(user_id: 1, difficulty: "easy", score: "01:05.000")
+Game.create(user_id: 2, difficulty: "easy", score: "00:10.000")
+Game.create(user_id: 3, difficulty: "easy", score: "00:05.000")
+
+
+
 # Image.create(name: "Cereal", img_url: "https://www.kiss917.com/wp-content/uploads/sites/4/2017/05/cereal-952x576.jpg")
 # Image.create(name: "Cereal", img_url: "https://www.kiss917.com/wp-content/uploads/sites/4/2017/05/cereal-952x576.jpg")
 # Image.create(name: "Cereal", img_url: "https://www.kiss917.com/wp-content/uploads/sites/4/2017/05/cereal-952x576.jpg")
