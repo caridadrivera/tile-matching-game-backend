10f4fc20125c82df2bc4bade34213657da42bd27
Jack Hilscher <Jack@Jacks-MacBook-Air.local>
2019-05-08 20:34:12 -0400

    fixed schema, and seed data

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -11,7 +11,7 @@ class Api::V1::UsersController < ApplicationController
 
   def create
     # byebug
-    user = User.create(user_params)
+    user = User.find_or_create_by(user_params)
     render json: user
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_06_151732) do
+ActiveRecord::Schema.define(version: 2019_05_08_225405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "games", force: :cascade do |t|
     t.integer "user_id"
-    t.integer "score"
     t.string "difficulty"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "score"
   end
 
   create_table "images", force: :cascade do |t|
